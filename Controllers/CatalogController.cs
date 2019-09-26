3c2b43f21c322a594fdbd457bfe0d2c676a244e3
Sergio Inzunza <inxunxa@gmail.com>
2019-09-25 17:31:20 -0700

    Register page created, still empty

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CarRental.Models;
 
 namespace CarRental.Controllers
 {
@@ -12,13 +14,37 @@ namespace CarRental.Controllers
         }
 
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
         [HttpGet]
         // Endpoint to get the list of cars to show on catalog page
         public IActionResult GetCatalog()
         {
-            string test = "JSON Working";
+            List<Car> cars = new List<Car>();
 
-            return Json(test);
+            Car c1 = new Car();
+            c1.Id = 1;
+            c1.Color = "Red";
+            c1.Cyls = 8;
+            c1.DailyPrice = 57.98m;
+            c1.Description = "Super conffy and reliable car on a cheap price";
+            c1.Doors = 2;
+            c1.HP = 495;
+            c1.ImageURL = "https://www.popsci.com/resizer/m-PYL9ERA2YASYR2tXzwE87Pw-E=/1015x1280/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/6GOP7VYO3DO5KHYK7RCRXVLQRU.jpg";
+            c1.Make = "Chevy";
+            c1.Mileage = 29;
+            c1.Model = "Corvette";
+            c1.Seats = 2;
+            c1.Year = 2020;
+
+            cars.Add(c1);
+
+
+            return Json(cars);
         }
 
     }
--- wwwroot/js/catalog.js
+++ wwwroot/js/catalog.js
@@ -6,12 +6,44 @@ function getCatalogData() {
         url: '/catalog/getCatalog',
         type: 'GET',
         success: function (res) {
-            console.log("Server says", res);
+            console.log(res);
+            for (i = 0; i < res.length; i++) {
+                displayCar(res[i]);
+            }
         },
         error: function (error) {
             console.log("Erron getting catalog", error);
         }
     });
+}
+
+function displayCar(car) {
+    var container = $("#catContainer");
+
+    var template = `
+        <div class='card col-6'>
+            <div class='row'>
+                <div class='col-7'>
+                    <img class='car-img' src='${car.imageURL}'>
+                </div>
+
+                <div class="col-5">
+                <div class="card-body">
+                <h5 class="card-title">${car.make} ${car.model}</h5>
+                <p class="card-text">${car.description}</p>
+                <p class="card-text"><small class="text-muted">HP:${car.hp}</small></p>
+                <p class="card-text"><span class="price">$ ${car.dailyPrice}</span> 
+                   <button class="btn btn-primary float-right" onclick="rent(${car.id})">Rent Me</button> 
+                </p>
+            </div>
+                
+                </div>
+            </div>            
+        </div>
+    `;
+
+    container.append(template);
+
 }
 
 
